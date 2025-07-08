5bb103a454440a297df46328ddc6e9baac5b1f4b
leurux-lex <hayagriva750@gmail.com>
2025-07-08 19:50:11 +0530

    ai marks grading and ai chatbot 0.01v

---
+++ chatbot 0.01v.py
@@ -0,0 +1,11 @@
+import time
+chat =input("").lower()
+
+if chat == "hello":
+    print ("hello , user whats upp may i help you some thing in special💝.")
+elif chat == "bye":
+    print ("may i see you when you need help👋")
+elif chat == "time":
+    print ("now actuall time is:",time.ctime())
+else:
+    print ("bot:i mean iam actually in development so it needs little bit upgrade🏗️")
---
+++ schoolgradermarksai.py
@@ -0,0 +1,12 @@
+marks = int(input("enter you marks so i can give you grade:"))
+
+if marks >= 500:
+    print ("well score so grade A so top🔥")
+elif marks >= 400:
+    print ("not so top but your grade is B⚡")
+elif marks >= 300:
+    print ("medium grade but still keep it up your grade C❄️")
+elif marks >= 200:
+    print ("your score is a bit less than medium so grade D✅")
+elif marks >= 100:
+    print ("your score is way to weak to pass so your failed💔")
