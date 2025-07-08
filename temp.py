9fd2e7aff06d72df5119dd53f717b8d070d82093
leurux-lex <hayagriva750@gmail.com>
2025-07-08 18:38:19 +0530

    login system with secure code for passport✅

---
+++ temp.py
@@ -0,0 +1,14 @@
+import time
+for i in range (3):
+    name = input ("enter your name:")
+    age = input ("enter your age:")
+    country = input ("enter you country:")
+    code = input ("enter your code:")
+    break
+if code == "78917":
+  print("logined:✅",name)
+  print("at time:",time.ctime())
+  print("good passport like rocking always⚡")
+else:
+   for i in range (3):
+    print ("some thing wrong check again❌")
